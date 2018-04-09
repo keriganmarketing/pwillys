5c8debeb3799b095c33bc8f04c87b70b2619f971
programmer-man <bryan@kerigan.com>
2018-04-09 17:36:30 -0400

    temp hide instagram

--- wp-content/themes/kma-slim/template-parts/partials/instagram-module.php
+++ wp-content/themes/kma-slim/template-parts/partials/instagram-module.php
@@ -1,17 +1,17 @@
 <?php
-use Includes\Modules\KMAInstagram\InstagramController;
-
-$instagram = new InstagramController();
-$photos = $instagram->getFeed(9);
-
-if(count(json_decode($photos))>0){
-?>
-<div class="card social-module instagram has-text-centered">
-    <div class="card-content">
-        <insta-gallery :photos=<?= $photos; ?>></insta-gallery>
-    </div>
-    <div class="card-bottom">
-        <a target="_blank" href="https://www.instagram.com/pwillys/" class="button is-primary is-large is-rounded has-shadow">View more on Instagram</a>
-    </div>
-</div>
-<?php } ?>
+//use Includes\Modules\KMAInstagram\InstagramController;
+//
+//$instagram = new InstagramController();
+//$photos = $instagram->getFeed(9);
+//
+//if(count(json_decode($photos))>0){
+//?>
+<!--<div class="card social-module instagram has-text-centered">-->
+<!--    <div class="card-content">-->
+<!--        <insta-gallery :photos=--><?//= $photos; ?><!--></insta-gallery>-->
+<!--    </div>-->
+<!--    <div class="card-bottom">-->
+<!--        <a target="_blank" href="https://www.instagram.com/pwillys/" class="button is-primary is-large is-rounded has-shadow">View more on Instagram</a>-->
+<!--    </div>-->
+<!--</div>-->
+<?php //} ?>
