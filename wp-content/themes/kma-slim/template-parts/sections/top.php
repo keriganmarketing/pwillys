d5584c95df79a54f136b46a5f88df9700017bfa2
Bryan Baird <bryan@kerigan.com>
2018-11-26 16:03:30 -0500

    add seasonal closing message

--- wp-content/themes/kma-slim/template-parts/sections/top.php
+++ wp-content/themes/kma-slim/template-parts/sections/top.php
@@ -36,8 +36,8 @@ use Includes\Modules\Helpers\PageField;
         'full-height': footerStuck,
         'scrolling': isScrolling }
     ">
-        <div id="alert" class="bg-primary p-2 text-white">
-        Closed for the season to make repairs from Hurricane Michael. Enjoy our beach cams while we prepare for the 2019 season.
+        <div id="alert" class="bg-primary p-2 text-white text-center">
+        <p class="m-0">Closed for the season to make repairs from Hurricane Michael. Enjoy our beach cams while we prepare for the 2019 season.</p>
         </div> 
         <div id="top" class="header">
             <div class="top-one">
