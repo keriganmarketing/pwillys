d9a06706055ef815ddb231041a6351542f45049b
Bryan Baird <23462829+programmer-man@users.noreply.github.com>
2021-12-03 09:28:06 -0600

    set admin email on donation form

--- wp-content/themes/kma-slim/inc/modules/Forms/Donations.php
+++ wp-content/themes/kma-slim/inc/modules/Forms/Donations.php
@@ -43,7 +43,7 @@ class Donations extends Form {
 
   public $restRoute = '/submit-donation-request';
 
-  public $mailto = 'bryan@kerigan.com';
+  public $mailto = 'jaredknetzer@gmail.com';
   public $mailcc = '';
   public $mailbcc = 'websites@kerigan.com';
   public $mailfrom = 'donations@mg.pwillys.com';
